6f8397add41f3feb9b6e14b4ec6a98f7dd693176
LuisFel123 <felipe_ramirez1234@hotmail.com>
2026-03-01 23:39:14 -0600

    crud

---
+++ curso-kubernetes/curso-kubernetes/msvc-usuarios/src/main/java/org/arreortua/springcloud/msvc/usuarios/msvc_usuarios/repositories/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package org.arreortua.springcloud.msvc.usuarios.msvc_usuarios.repositories;
+
+import org.arreortua.springcloud.msvc.usuarios.msvc_usuarios.models.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario,Long>{
+}
