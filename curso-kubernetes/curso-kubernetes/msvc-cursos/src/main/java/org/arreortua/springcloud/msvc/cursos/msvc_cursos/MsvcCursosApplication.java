9717397035d3f71c76991d558bfb30d1bb8fb3f6
LuisFel123 <felipe_ramirez1234@hotmail.com>
2026-04-06 12:35:57 -0600

    cambios

---
+++ curso-kubernetes/curso-kubernetes/msvc-cursos/src/main/java/org/arreortua/springcloud/msvc/cursos/msvc_cursos/MsvcCursosApplication.java
@@ -0,0 +1,13 @@
+package org.arreortua.springcloud.msvc.cursos.msvc_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCursosApplication.class, args);
+	}
+
+}
