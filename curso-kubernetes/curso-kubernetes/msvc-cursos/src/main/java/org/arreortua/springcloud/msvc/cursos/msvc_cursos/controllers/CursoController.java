78f8bea11cf12c459e0b8009deb7917c9b7354fb
LuisFel123 <felipe_ramirez1234@hotmail.com>
2026-04-28 17:18:32 -0600

    cambios

--- curso-kubernetes/curso-kubernetes/msvc-cursos/src/main/java/org/arreortua/springcloud/msvc/cursos/msvc_cursos/controllers/CursoController.java
+++ curso-kubernetes/curso-kubernetes/msvc-cursos/src/main/java/org/arreortua/springcloud/msvc/cursos/msvc_cursos/controllers/CursoController.java
@@ -1,9 +1,14 @@
 package org.arreortua.springcloud.msvc.cursos.msvc_cursos.controllers;
 
+import org.arreortua.springcloud.msvc.cursos.msvc_cursos.entity.Curso;
 import org.arreortua.springcloud.msvc.cursos.msvc_cursos.services.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CursoController {
@@ -12,4 +17,55 @@ public class CursoController {
     private CursoService service;
 
 
+    //listar
+    @GetMapping
+    public ResponseEntity<List<Curso>> listar(){
+        return ResponseEntity.ok(service.listar());
+    }
+
+
+    //por id
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable Long id){
+
+        Optional<Curso> o = service.porId(id);
+        if(o.isPresent()){
+            return ResponseEntity.ok(o.get());
+
+        }
+        return  ResponseEntity.notFound().build();
+
+    }
+
+
+    //post insertar un curso
+    @PostMapping("/")
+    public ResponseEntity<?> crear(@RequestBody Curso curso){
+        Curso cursoDb = service.guardar(curso);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cursoDb);
+
+    }
+
+    //put editar un curso
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Curso curso, @PathVariable Long id){
+        Optional <Curso> o = service.porId(id);
+        if(o.isPresent()){
+            Curso cursoDb= o.get();
+            cursoDb.setNombre(curso.getNombre());
+            return  ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(cursoDb));
+        }
+            return ResponseEntity.notFound().build();
+    }
+
+    // delete eliminar un curso
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        Optional<Curso> o = service.porId(id);
+        if(o.isPresent()){
+            service.eliminar(o.get().getId());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
